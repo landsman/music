9a1b47f4d0fdc175e83775cfb1beabe9d4f4365a
Michal Landsman <landsman@studioart.cz>
2025-02-01 13:39:29 +0100

    deno fmt

--- supabase/functions/fetch-scrobbles/env.ts
+++ supabase/functions/fetch-scrobbles/env.ts
@@ -1,6 +1,6 @@
 export const env = {
-    SUPABASE_URL: Deno.env.get("SUPABASE_URL")!,
-    SUPABASE_ANON_KEY: Deno.env.get("SUPABASE_ANON_KEY")!,
-    LASTFM_API_KEY: Deno.env.get("LASTFM_API_KEY")!,
-    LASTFM_USERNAME: Deno.env.get("LASTFM_USERNAME")!,
-}
+  SUPABASE_URL: Deno.env.get("SUPABASE_URL")!,
+  SUPABASE_ANON_KEY: Deno.env.get("SUPABASE_ANON_KEY")!,
+  LASTFM_API_KEY: Deno.env.get("LASTFM_API_KEY")!,
+  LASTFM_USERNAME: Deno.env.get("LASTFM_USERNAME")!,
+};
--- supabase/functions/fetch-scrobbles/index.ts
+++ supabase/functions/fetch-scrobbles/index.ts
@@ -1,84 +1,104 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import { getRecentTracks } from "./lastfm.ts"
-import { Row, TableListened } from "./table.ts"
-import { env } from "./env.ts"
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { getRecentTracks } from "./lastfm.ts";
+import { Row, TableListened } from "./table.ts";
+import { env } from "./env.ts";
 
 Deno.serve(async () => {
-    const size = 50
-    const supabaseClient = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
-    const table = new TableListened(supabaseClient)
-    const startFrom: number | null = await table.getLastListenedDate()
-    let totalPages = 1
-    let total = 0
-    let page = 1
+  const size = 50;
+  const supabaseClient = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+  const table = new TableListened(supabaseClient);
+  const startFrom: number | null = await table.getLastListenedDate();
+  let totalPages = 1;
+  let total = 0;
+  let page = 1;
 
-    if (startFrom) {
-        console.log('Starting from last listened date:', new Date(startFrom * 1000).toLocaleString())
+  if (startFrom) {
+    console.log(
+      "Starting from last listened date:",
+      new Date(startFrom * 1000).toLocaleString(),
+    );
+  } else {
+    console.log("Database is empty, starting from the last page");
+  }
+
+  const fmInitial = await getRecentTracks(
+    env.LASTFM_API_KEY,
+    env.LASTFM_USERNAME,
+    1,
+    size,
+    startFrom,
+  );
+  if (fmInitial === null) {
+    console.error("Fail - No tracks returned from initial api request.");
+    return new Response("error", { headers: { "Content-Type": "text/plain" } });
+  }
+  const paginationInitial = fmInitial.recenttracks["@attr"];
+  totalPages = parseInt(paginationInitial.totalPages);
+  total = parseInt(paginationInitial.total);
+  page = totalPages;
+
+  if (startFrom) {
+    console.log(
+      `Found ${total} new tracks to save! Starting from page ${totalPages}.`,
+    );
+  } else {
+    console.log(
+      `Found ${total} tracks in total. Starting from page ${totalPages}.`,
+    );
+  }
+
+  let processedPages = 0;
+  do {
+    if (processedPages === 10) {
+      console.log(
+        `Already inserted ${
+          processedPages * size
+        } items to db. Stopped. See ya in next cron.`,
+      );
+      break;
     }
-    else {
-        console.log('Database is empty, starting from the last page')
+
+    const fm = await getRecentTracks(
+      env.LASTFM_API_KEY,
+      env.LASTFM_USERNAME,
+      page,
+      size,
+      startFrom,
+    );
+    if (fm === null) {
+      console.error("Fail - No tracks returned from api request.");
+      break;
+    }
+    const data = fm.recenttracks;
+    const tracks = data.track;
+
+    if (total === 0 || tracks.length === 0) {
+      break;
     }
 
-    const fmInitial = await getRecentTracks(env.LASTFM_API_KEY, env.LASTFM_USERNAME, 1, size, startFrom)
-    if (fmInitial === null) {
-        console.error('Fail - No tracks returned from initial api request.')
-        return new Response('error', { headers: { "Content-Type": "text/plain" } })
+    console.log(`Fetching page ${page}/${totalPages}`);
+
+    const toInsert: Row[] = tracks.map((track) => ({
+      created_at: new Date().toISOString(),
+      listened_at: new Date(track.date.uts * 1000).toISOString(),
+      artist_name: track.artist.name,
+      track_name: track.name,
+      album_name: track.album["#text"],
+      lastfm_data: track,
+    }));
+
+    const { error, message } = await table.save(toInsert);
+    if (error) {
+      break;
     }
-    const paginationInitial = fmInitial.recenttracks["@attr"]
-    totalPages = parseInt(paginationInitial.totalPages)
-    total = parseInt(paginationInitial.total)
-    page = totalPages
 
-    if (startFrom) {
-        console.log(`Found ${total} new tracks to save! Starting from page ${totalPages}.`)
-    }
-    else {
-        console.log(`Found ${total} tracks in total. Starting from page ${totalPages}.`)
+    if (message) {
+      console.log(message);
     }
 
-    let processedPages = 0
-    do {
-        if (processedPages === 10) {
-            console.log(`Already inserted ${processedPages * size} items to db. Stopped. See ya in next cron.`)
-            break
-        }
+    processedPages++;
+    page--;
+  } while (page >= 1);
 
-        const fm = await getRecentTracks(env.LASTFM_API_KEY, env.LASTFM_USERNAME, page, size, startFrom)
-        if (fm === null) {
-            console.error('Fail - No tracks returned from api request.')
-            break
-        }
-        const data = fm.recenttracks
-        const tracks = data.track
-
-        if (total === 0 || tracks.length === 0) {
-            break
-        }
-
-        console.log(`Fetching page ${page}/${totalPages}`)
-
-        const toInsert: Row[] = tracks.map(track => ({
-            created_at: new Date().toISOString(),
-            listened_at: new Date(track.date.uts * 1000).toISOString(),
-            artist_name: track.artist.name,
-            track_name: track.name,
-            album_name: track.album["#text"],
-            lastfm_data: track,
-        }))
-
-        const { error, message } = await table.save(toInsert)
-        if (error) {
-            break
-        }
-
-        if (message) {
-            console.log(message)
-        }
-
-        processedPages++
-        page--
-    }
-    while (page >= 1)
-
-    return new Response('ok1', { headers: { "Content-Type": "text/plain" } })
-})
+  return new Response("ok1", { headers: { "Content-Type": "text/plain" } });
+});
