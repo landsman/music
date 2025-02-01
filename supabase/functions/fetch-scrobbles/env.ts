0c6e05958fd2a37488264cbc729c84c239408600
Michal Landsman <landsman@studioart.cz>
2025-02-01 14:43:29 +0100

    add sentry for error notifications via email

--- supabase/functions/fetch-scrobbles/env.ts
+++ supabase/functions/fetch-scrobbles/env.ts
@@ -1,4 +1,11 @@
-export const env = {
+export interface Variables {
+  SUPABASE_URL: string;
+  SUPABASE_ANON_KEY: string;
+  LASTFM_API_KEY: string;
+  LASTFM_USERNAME: string;
+}
+
+export const env: Variables = {
   SUPABASE_URL: Deno.env.get("SUPABASE_URL")!,
   SUPABASE_ANON_KEY: Deno.env.get("SUPABASE_ANON_KEY")!,
   LASTFM_API_KEY: Deno.env.get("LASTFM_API_KEY")!,
