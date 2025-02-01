ee22aa88f110fd13a6124525ef4aa604e610d714
Michal Landsman <landsman@studioart.cz>
2025-02-01 14:48:44 +0100

    add sentry for error notifications via email (#2)

--- supabase/functions/fetch-scrobbles/index.ts
+++ supabase/functions/fetch-scrobbles/index.ts
@@ -1,106 +1,33 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { getRecentTracks } from "./lastfm.ts";
-import { Row, TableListened } from "./table.ts";
+import * as Sentry from "https://deno.land/x/sentry/index.mjs";
+import { scrobbles } from "./scrobbles.ts";
 import { env } from "./env.ts";
 
+Sentry.init({
+  dsn: Deno.env.get("SENTRY_DSN")!,
+  defaultIntegrations: false,
+  tracesSampleRate: 1.0,
+  // deno-lint-ignore ban-ts-comment
+  // @ts-ignore
+  profilesSampleRate: 1.0,
+});
+
+// Set region and execution_id as custom tags
+Sentry.setTag("region", Deno.env.get("SB_REGION"));
+Sentry.setTag("execution_id", Deno.env.get("SB_EXECUTION_ID"));
+Sentry.setTag("url", env.SUPABASE_URL);
+
 Deno.serve(async () => {
-  const size = 50;
-  const supabaseClient = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
-  const table = new TableListened(supabaseClient);
-  const startFrom: number | null = await table.getLastListenedDate();
-  let totalPages = 1;
-  let total = 0;
-  let page = 1;
-
-  if (startFrom) {
-    console.log(
-      "Starting from last listened date:",
-      new Date(startFrom * 1000).toLocaleString(),
-    );
-  } else {
-    console.log("Database is empty, starting from the last page");
+  try {
+    const result = await scrobbles(env);
+    return new Response(result, {
+      status: 200,
+      headers: { "Content-Type": "text/plain" },
+    });
+  } catch (e) {
+    Sentry.captureException(e);
+    return new Response("error occured, please check sentry/supabase logs", {
+      status: 500,
+      headers: { "Content-Type": "text/plain" },
+    });
   }
-
-  const fmInitial = await getRecentTracks(
-    env.LASTFM_API_KEY,
-    env.LASTFM_USERNAME,
-    1,
-    size,
-    startFrom,
-  );
-  if (fmInitial === null) {
-    console.error("Fail - No tracks returned from initial api request.");
-    return new Response("error", { headers: { "Content-Type": "text/plain" } });
-  }
-  const paginationInitial = fmInitial.recenttracks["@attr"];
-  totalPages = parseInt(paginationInitial.totalPages);
-  total = parseInt(paginationInitial.total);
-  page = totalPages;
-
-  if (startFrom) {
-    console.log(
-      `Found ${total} new tracks to save! Starting from page ${totalPages}.`,
-    );
-  } else {
-    console.log(
-      `Found ${total} tracks in total. Starting from page ${totalPages}.`,
-    );
-  }
-
-  let processedPages = 0;
-  do {
-    if (processedPages === 10) {
-      console.log(
-        `Already inserted ${
-          processedPages * size
-        } items to db. Stopped. See ya in next cron.`,
-      );
-      break;
-    }
-
-    const fm = await getRecentTracks(
-      env.LASTFM_API_KEY,
-      env.LASTFM_USERNAME,
-      page,
-      size,
-      startFrom,
-    );
-    if (fm === null) {
-      console.error("Fail - No tracks returned from api request.");
-      break;
-    }
-    const data = fm.recenttracks;
-    const tracks = data.track;
-
-    if (total === 0 || tracks.length === 0) {
-      break;
-    }
-
-    console.log(`Fetching page ${page}/${totalPages}`);
-
-    const toInsert: Row[] = tracks
-      .filter((track) => !(track["@attr"] && track["@attr"].nowplaying))
-      .map((track) => ({
-        created_at: new Date().toISOString(),
-        listened_at: new Date(track.date!.uts * 1000).toISOString(),
-        artist_name: track.artist.name,
-        track_name: track.name,
-        album_name: track.album["#text"],
-        lastfm_data: track,
-      }));
-
-    const { error, message } = await table.save(toInsert);
-    if (error) {
-      break;
-    }
-
-    if (message) {
-      console.log(message);
-    }
-
-    processedPages++;
-    page--;
-  } while (page >= 1);
-
-  return new Response("ok1", { headers: { "Content-Type": "text/plain" } });
 });
---
+++ supabase/functions/fetch-scrobbles/scrobbles.ts
@@ -0,0 +1,105 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { getRecentTracks } from "./lastfm.ts";
+import { Row, TableListened } from "./table.ts";
+import { Variables } from "./env.ts";
+
+export async function scrobbles(env: Variables): Promise<string> {
+  const size = 50;
+  const supabaseClient = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+  const table = new TableListened(supabaseClient);
+  const startFrom: number | null = await table.getLastListenedDate();
+  let totalPages = 1;
+  let total = 0;
+  let page = 1;
+
+  if (startFrom) {
+    console.log(
+      "Starting from last listened date:",
+      new Date(startFrom * 1000).toLocaleString(),
+    );
+  } else {
+    console.log("Database is empty, starting from the last page");
+  }
+
+  const fmInitial = await getRecentTracks(
+    env.LASTFM_API_KEY,
+    env.LASTFM_USERNAME,
+    1,
+    size,
+    startFrom,
+  );
+  if (fmInitial === null) {
+    throw new Error("Fail - No tracks returned from initial api request.");
+  }
+  const paginationInitial = fmInitial.recenttracks["@attr"];
+  totalPages = parseInt(paginationInitial.totalPages);
+  total = parseInt(paginationInitial.total);
+  page = totalPages;
+
+  if (startFrom) {
+    console.log(
+      `Found ${total} new tracks to save! Starting from page ${totalPages}.`,
+    );
+  } else {
+    console.log(
+      `Found ${total} tracks in total. Starting from page ${totalPages}.`,
+    );
+  }
+
+  let processedPages = 0;
+  do {
+    if (processedPages === 10) {
+      console.log(
+        `Already inserted ${
+          processedPages * size
+        } items to db. Stopped. See ya in next cron.`,
+      );
+      break;
+    }
+
+    const fm = await getRecentTracks(
+      env.LASTFM_API_KEY,
+      env.LASTFM_USERNAME,
+      page,
+      size,
+      startFrom,
+    );
+    if (fm === null) {
+      console.warn("Fail - No tracks returned from api request.");
+      break;
+    }
+    const data = fm.recenttracks;
+    const tracks = data.track;
+
+    if (total === 0 || tracks.length === 0) {
+      break;
+    }
+
+    console.log(`Fetching page ${page}/${totalPages}`);
+
+    const toInsert: Row[] = tracks
+      .filter((track) => !(track["@attr"] && track["@attr"].nowplaying))
+      .map((track) => ({
+        created_at: new Date().toISOString(),
+        listened_at: new Date(track.date!.uts * 1000).toISOString(),
+        artist_name: track.artist.name,
+        track_name: track.name,
+        album_name: track.album["#text"],
+        lastfm_data: track,
+      }));
+
+    const { error, message } = await table.save(toInsert);
+    if (error) {
+      throw new Error(error.toString());
+    }
+
+    if (message) {
+      console.log(message);
+    }
+
+    processedPages++;
+    page--;
+  } while (page >= 1);
+
+  return "ok";
+}
