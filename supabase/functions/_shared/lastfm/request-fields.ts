c3c9f0fbab7ec85684da651bbfbfd1152acc4138
Michal Landsman <landsman@studioart.cz>
2025-02-02 17:06:16 +0100

    throw error when the request body is missing

--- supabase/functions/_shared/lastfm/request-fields.ts
+++ supabase/functions/_shared/lastfm/request-fields.ts
@@ -3,5 +3,8 @@
  */
 export async function getLastFmUser(req: Request, fallbackTo: string) {
   const body = await req.json();
+  if (body === undefined || body === null) {
+    throw new Error("Request missing request body");
+  }
   return typeof body.lastFmUser === "string" ? body.lastFmUser : fallbackTo;
 }
