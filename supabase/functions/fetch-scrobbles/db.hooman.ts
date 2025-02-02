0666ce2ebc8941ad153cc4cac62ca565fa50f828
Michal Landsman <landsman@studioart.cz>
2025-02-02 11:06:44 +0100

    migrate existing data, find tracks in database by user (#5)

--- supabase/functions/fetch-scrobbles/db.hooman.ts
+++ supabase/functions/fetch-scrobbles/db.hooman.ts
@@ -17,19 +17,19 @@ export class TableHooman extends DbSupabaseTable {
     const { data, error } = await this.getSupabase()
       .from(this.tableName)
       .select("id")
-      .eq(columnName.lastfm_user, lastFmUser)
+      .eq(columnName.lastfm_user, lastFmUser.replace(/\s/g, ""))
       .limit(1)
       .maybeSingle<{ id: string } | null>();
 
     if (error) {
-      throw new Error("Error fetching hooman: " + JSON.stringify(error));
+      throw new Error("Error fetching Hooman: " + JSON.stringify(error));
     }
 
-    if (data) {
+    if (data !== null) {
       return data.id;
+    } else {
+      console.log("Hooman not found:", lastFmUser);
     }
-
-    console.log(`Creating new user: ${lastFmUser}`);
 
     const { data: insertData, error: insertError } = await this.getSupabase()
       .from(this.tableName)
@@ -41,8 +41,10 @@ export class TableHooman extends DbSupabaseTable {
       .maybeSingle<{ id: string }>();
 
     if (insertError || !insertData) {
-      throw new Error("Error creating hooman: " + JSON.stringify(insertError));
+      throw new Error("Error creating Hooman: " + JSON.stringify(insertError));
     }
+
+    console.log("Created new Hooman:", insertData);
 
     return insertData.id;
   }
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -6,8 +6,18 @@ CREATE TABLE listened
     artist_name TEXT        NOT NULL,
     track_name  TEXT        NOT NULL,
     album_name  TEXT,
-    lastfm_data jsonb DEFAULT NULL
+    lastfm_data jsonb DEFAULT NULL,
+    hooman_id   UUID NOT NULL
 );
 
 CREATE INDEX idx_created_at ON listened (created_at DESC);
 CREATE INDEX idx_listened_at ON listened (listened_at DESC);
+
+CREATE TABLE hooman
+(
+    id          UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    created_at  TIMESTAMPTZ NOT NULL,
+    lastfm_user TEXT        NOT NULL
+);
+
+CREATE INDEX idx_lastfm_user ON hooman (lastfm_user DESC);
