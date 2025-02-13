454b0e7c77600ee53004d34769e9a867902c2890
Michal Landsman <landsman@studioart.cz>
2025-02-13 23:07:29 +0100

    update migration

--- supabase/migrations/20250213214107_new.sql
+++ supabase/migrations/20250213214107_new.sql
@@ -3,3 +3,15 @@ ALTER TABLE listened
     ADD CONSTRAINT fk_listened_hooman
         FOREIGN KEY (hooman_id)
             REFERENCES hooman (id);
+
+ALTER TABLE hooman_artist
+    DROP CONSTRAINT IF EXISTS fk_hooman_artist_hooman,
+    ADD CONSTRAINT fk_hooman_artist_hooman
+        FOREIGN KEY (hooman_id)
+            REFERENCES hooman (id);
+
+ALTER TABLE hooman_artist
+    DROP CONSTRAINT IF EXISTS fk_hooman_artist_artist,
+    ADD CONSTRAINT fk_hooman_artist_artist
+        FOREIGN KEY (artist_id)
+            REFERENCES artist (id);
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -65,6 +65,18 @@ ALTER TABLE hooman_artist
 
 CREATE INDEX idx_hooman_artist_hooman_id ON hooman_artist (hooman_id DESC);
 
+ALTER TABLE hooman_artist
+    DROP CONSTRAINT IF EXISTS fk_hooman_artist_hooman,
+    ADD CONSTRAINT fk_hooman_artist_hooman
+        FOREIGN KEY (hooman_id)
+            REFERENCES hooman (id);
+
+ALTER TABLE hooman_artist
+    DROP CONSTRAINT IF EXISTS fk_hooman_artist_artist,
+    ADD CONSTRAINT fk_hooman_artist_artist
+        FOREIGN KEY (artist_id)
+            REFERENCES artist (id);
+
 CREATE VIEW hooman_artist_count AS
 SELECT
     h.lastfm_user,
