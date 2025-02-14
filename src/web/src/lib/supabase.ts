315fc7950ff1861ec6fcbffc7840c6f28bb349a1
Michal Landsman <landsman@studioart.cz>
2025-02-14 11:33:23 +0100

    types

--- src/web/src/lib/supabase.ts
+++ src/web/src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-import type {Database} from "../../../shared/db.types.ts";
+import type { Database } from "../../../shared/db.types.ts";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export type { Database };
--- src/web/src/tracks/api.ts
+++ src/web/src/tracks/api.ts
@@ -1,30 +1,28 @@
-import {supabase} from "../lib/supabase.ts";
+import { supabase, Database } from "../lib/supabase.ts";
 
-export interface LastListenedTrack {
-    id: string;
-    artist_name: string;
-    track_name: string;
-    hooman: {
-        id: string;
-        lastfm_user: string;
-    } | null; // todo: get rid of nullable in db
+// Define type for the database tables
+type Tables = Database['public']['Tables'];
+type Listened = Tables['listened']['Row'];
+type Hooman = Tables['hooman']['Row'];
+
+export interface LastListenedTrack extends Listened {
+    hooman: Hooman;
 }
 
 export async function getLastListenedTracks(): Promise<LastListenedTrack[]> {
     const { data, error } = await supabase
-        .from("listened")
-        .select(`
-        id, 
-        artist_name, 
-        track_name, 
-        hooman (
-          id,
-          lastfm_user
-        )
-      `)
+        .from('listened')
+        .select<string, LastListenedTrack>(`
+            id, 
+            artist_name, 
+            track_name,
+            hooman (
+              id,
+              lastfm_user
+            )
+        `)
         .order("listened_at", { ascending: false })
-        .limit(50)
-    ;
+        .limit(50);
 
     if (error) {
         throw error;
