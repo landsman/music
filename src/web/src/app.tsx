e59f2aef7c44ce1e02c34c375f56f0379226a6f8
Michal Landsman <landsman@studioart.cz>
2025-02-14 17:41:14 +0100

    copy to clipboard

--- src/web/src/app.tsx
+++ src/web/src/app.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import {Toaster} from "react-hot-toast";
 import {ReactQuery} from "./lib/react-query.tsx";
 import {IndexView} from "./view/index-view.tsx";
 import {Header} from "./ui/header.tsx";
 import {Footer} from "./ui/footer.tsx";
 import {Container} from "./ui/container.tsx";
-import './ui/app.css'
+import "./ui/app.css";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Header />
         <IndexView />
         <Footer />
+        <Toaster />
       </Container>
     </ReactQuery>
   );
--- src/web/src/ui/feed/feed-item.tsx
+++ src/web/src/ui/feed/feed-item.tsx
@@ -4,6 +4,7 @@ import {
 } from "../../lib/localize-date.ts";
 import {i18n} from "../../i18n/i18n.ts";
 import {Marquee} from "./track.tsx";
+import toast from "react-hot-toast";
 
 interface Props {
   artist: string;
@@ -14,9 +15,19 @@ interface Props {
 
 export function FeedItem(props: Props) {
   const { artist, track, user, listenedAt } = props;
+
+  function handleOnClick(e: React.MouseEvent<HTMLDivElement>) {
+      e.preventDefault();
+      toast.promise(navigator.clipboard.writeText(`${artist} - ${track}`), {
+          loading: 'Copy to clipboard...',
+          success: 'track copied, I bet you\'ll like it',
+          error: 'Error during copy...',
+      });
+  }
+
   return (
     <div className="feed_row">
-      <div className="track_side">
+      <div className="track_side" onClick={handleOnClick}>
         <div className="track">
             <Marquee text={track} />
         </div>
