f182de441e1061ba8f2b57d8594206a41e5bcaff
Michal Landsman <landsman@studioart.cz>
2025-02-14 11:40:14 +0100

    better type syntax

--- src/web/src/tracks/api.ts
+++ src/web/src/tracks/api.ts
@@ -5,18 +5,21 @@ type Tables = Database['public']['Tables'];
 type Listened = Tables['listened']['Row'];
 type Hooman = Tables['hooman']['Row'];
 
-export interface LastListenedTrack extends Listened {
+type JoinedListened = Omit<Listened, 'hooman_id'> & {
     hooman: Hooman;
-}
+};
 
-export async function getLastListenedTracks(): Promise<LastListenedTrack[]> {
+export async function getLastListenedTracks(): Promise<JoinedListened[]> {
     const { data, error } = await supabase
         .from('listened')
-        .select<string, LastListenedTrack>(`
+        .select(`
             id, 
             artist_name, 
             track_name,
-            hooman (
+            album_lastfm_id,
+            album_name,
+            created_at,b
+            hooman:hooman_id (
               id,
               lastfm_user
             )
@@ -28,5 +31,9 @@ export async function getLastListenedTracks(): Promise<LastListenedTrack[]> {
         throw error;
     }
 
-    return data;
+    if (!data) {
+        throw new Error("No data returned");
+    }
+
+    return data as JoinedListened[];
 }
