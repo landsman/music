9410c2b8ef509a5a1f10b05b5eb3575b88551647
Michal Landsman <landsman@studioart.cz>
2025-05-31 14:35:28 +0200

    unique file

--- .github/action/deno-outdated/deno-outdated.sh
+++ .github/action/deno-outdated/deno-outdated.sh
@@ -3,21 +3,25 @@
 # This script checks for outdated Deno dependencies and formats the output for GitHub Actions
 # It removes ANSI color codes and formats the result as a code block
 
+
+TMP_FILE="outdated_$(date +%s)_$RANDOM.md"
+
 # Check for outdated dependencies with quiet and recursive flags
 OUTDATED="$(deno outdated -qr)"
 echo "$OUTDATED";
 
 # If no outdated dependencies are found, output "none", otherwise format the output
 if [ -z "$OUTDATED" ]; then
-  echo "none" > outdated.md
+  echo "none" > "$TMP_FILE"
 else
+  echo "yeah"
   # Remove ANSI color codes from the output
-  echo "$OUTDATED" | sed -r 's/\x1B\[[0-9;]*[mK]//g' > outdated.md
+  echo "$OUTDATED" | sed -r 's/\x1B\[[0-9;]*[mK]//g' > "$TMP_FILE"
 fi
 
 # Add the result to GitHub Actions output as a code block
 echo "result<<EOF" >> "$GITHUB_OUTPUT"
 echo '```text' >> "$GITHUB_OUTPUT"
-cat outdated.md >> "$GITHUB_OUTPUT"
+cat "$TMP_FILE" >> "$GITHUB_OUTPUT"
 echo '```' >> "$GITHUB_OUTPUT"
 echo "EOF" >> "$GITHUB_OUTPUT"
