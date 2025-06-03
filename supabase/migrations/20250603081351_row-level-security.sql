3fc6b5e3da35c402a6409a609e58bc95d1e6e4b1
Michal Landsman <landsman@studioart.cz>
2025-06-03 11:17:53 +0200

    improve migration

--- supabase/migrations/20250603081351_row-level-security.sql
+++ supabase/migrations/20250603081351_row-level-security.sql
@@ -4,23 +4,27 @@
 BEGIN;
 
 ALTER TABLE public.listened ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Allow unauthenticated access"
+DROP POLICY IF EXISTS "Allow unauthenticated access" ON public.listened;
+CREATE POLICY "select_public"
     ON public.listened
     FOR SELECT
     USING (true);
 
 ALTER TABLE public.hooman ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Allow public read access" ON public.hooman
+DROP POLICY IF EXISTS "Allow unauthenticated access" ON public.hooman;
+CREATE POLICY "select_public" ON public.hooman
     FOR SELECT
     USING (true);
 
 ALTER TABLE public.hooman_artist ENABLE ROW LEVEL SECURITY;
+DROP POLICY IF EXISTS "select_public" ON public.hooman_artist;
 CREATE POLICY select_public ON public.hooman_artist
     FOR SELECT
     USING (true);
 
 ALTER TABLE public.artist ENABLE ROW LEVEL SECURITY;
-CREATE POLICY select_artist ON public.artist
+DROP POLICY IF EXISTS "select_public" ON public.artist;
+CREATE POLICY select_public ON public.artist
     FOR SELECT
     USING (true);
 
