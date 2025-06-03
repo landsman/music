05a6ee6bd358d9362c4eb4e018acaccddc98ea53
Michal Landsman <landsman@studioart.cz>
2025-06-03 10:25:10 +0200

    migration - add other tables

--- supabase/migrations/20250603081351_row-level-security.sql
+++ supabase/migrations/20250603081351_row-level-security.sql
@@ -1,12 +1,28 @@
+-- Enable Row Level Security on the listened table
+-- Create a policy to allow unauthenticated access
+
 BEGIN;
 
--- Enable Row Level Security on the listened table
 ALTER TABLE public.listened ENABLE ROW LEVEL SECURITY;
-
--- Create a policy to allow unauthenticated access
 CREATE POLICY "Allow unauthenticated access"
     ON public.listened
     FOR SELECT
     USING (true);
 
+ALTER TABLE public.hooman ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "Allow public read access" ON public.hooman
+    FOR SELECT
+    USING (true);
+
+ALTER TABLE public.hooman_artist ENABLE ROW LEVEL SECURITY;
+CREATE POLICY select_public ON public.hooman_artist
+    FOR SELECT
+    USING (true);
+
+ALTER TABLE public.artist ENABLE ROW LEVEL SECURITY;
+CREATE POLICY select_artist ON public.artist
+    FOR SELECT
+    USING (true);
+
 COMMIT;
+
