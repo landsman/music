9983cfcae8b283974503457982c9f0a8709107ff
Michal Landsman <landsman@studioart.cz>
2026-03-11 12:08:08 +0100

    use service role key for edge functions (#76)

--- supabase/functions/_shared/env.ts
+++ supabase/functions/_shared/env.ts
@@ -2,6 +2,7 @@ export interface Variables {
   DEVELOPER_MODE: boolean;
   SUPABASE_URL: string;
   SUPABASE_ANON_KEY: string;
+  SUPABASE_SERVICE_ROLE_KEY: string;
   LASTFM_API_KEY: string;
   LASTFM_USERNAME: string;
 }
@@ -10,6 +11,7 @@ export const env: Variables = {
   DEVELOPER_MODE: Deno.env.get("DEVELOPER_MODE") !== "true",
   SUPABASE_URL: Deno.env.get("SUPABASE_URL")!,
   SUPABASE_ANON_KEY: Deno.env.get("SUPABASE_ANON_KEY")!,
+  SUPABASE_SERVICE_ROLE_KEY: Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
   LASTFM_API_KEY: Deno.env.get("LASTFM_API_KEY")!,
   LASTFM_USERNAME: Deno.env.get("LASTFM_USERNAME")!,
 };
